a536b409461dd5f8751af7c66f06c3f493e86181
Andre Peixoto <peixoto.amp@gmail.com>
2022-05-03 00:29:04 -0300

    Add initial functionality to Widget component

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,10 +1,22 @@
 import { ChatTeardropDots } from 'phosphor-react';
+import { useState } from 'react';
 
 export const Widget = () => {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+  const toggleWidgetVisibility = () => {
+    setIsWidgetOpen(!isWidgetOpen);
+  };
+
   return (
     <div className="button-container">
-      <button className="button">
+      {isWidgetOpen && <p>Hello World</p>}
+
+      <button onClick={toggleWidgetVisibility} className="button group">
         <ChatTeardropDots className="button-icon" />
+        <span className="button-title">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
       </button>
     </div>
   );
