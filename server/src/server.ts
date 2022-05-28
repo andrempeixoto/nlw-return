48b5e5452dfb1dc48ea7b97b648faac90a99b349
Andre Peixoto <peixoto.amp@gmail.com>
2022-05-27 23:52:35 -0300

    Create Feedback creation endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,22 @@
+import { prisma } from './prisma';
 import express from "express";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.send("/")
+app.use(express.json());
+
+app.post("/feedbacks", async (request, response) => {
+  const { type, comment, screenshot } = request.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+  return response.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
