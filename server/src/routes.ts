eeac580cfc8eb14b3486eeedc8576546e84d3c98
Andre Peixoto <peixoto.amp@gmail.com>
2022-06-03 23:59:40 -0300

    Install and use nodemailer to send email when feedback is received

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,17 @@
 import express from "express";
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma';
 
 export const routes = express.Router();
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "500e704fa4785b",
+    pass: "2a04427b3198d9"
+  }
+});
 
 routes.post("/feedbacks", async (request, response) => {
   const { type, comment, screenshot } = request.body;
@@ -12,6 +22,18 @@ routes.post("/feedbacks", async (request, response) => {
       comment,
       screenshot,
     }
+  })
+
+  transport.sendMail({
+    from: "Feedback <feedback@andrepeixoto.dev>",
+    to: "Andre Peixoto <hi@andrepeixoto.dev>",
+    subject: "New Feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;"`,
+      `<p>Feedback type: ${type}`,
+      `<p>Comment: ${comment}`, 
+      `</div>`
+    ].join('\n')
   })
 
   return response.status(201).json({ data: feedback });
